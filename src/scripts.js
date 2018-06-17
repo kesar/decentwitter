6ea33fe0c573efb97e062aa0c501e20448482f60
Cesar Rodriguez <kesarr@gmail.com>
2018-06-17 15:21:03 +0200

    🚀

--- src/scripts.js
+++ src/scripts.js
@@ -1,36 +1,9 @@
 $(function () {
-		$('#side-menu').metisMenu()
+    $('#side-menu').metisMenu({toggle: false});
 })
 
 $(function () {
-		$(window).bind('load resize', function () {
-				var topOffset = 50
-				var width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width
-				if (width < 768) {
-						$('div.navbar-collapse').addClass('collapse')
-						topOffset = 100 // 2-row-menu
-				} else {
-						$('div.navbar-collapse').removeClass('collapse')
-				}
-
-				var height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1
-				height = height - topOffset
-				if (height < 1) height = 1
-				if (height > topOffset) {
-						$('#page-wrapper').css('min-height', (height) + 'px')
-				}
-		})
-
-		var url = window.location
-		var element = $('ul.nav a').filter(function () {
-				return this.href === url
-		}).addClass('active').parent()
-
-		while (true) {
-				if (element.is('li')) {
-						element = element.parent().addClass('in').parent()
-				} else {
-						break
-				}
-		}
+    $(window).bind('load resize', function () {
+        $('div.navbar-collapse').addClass('collapse');
+    })
 })
