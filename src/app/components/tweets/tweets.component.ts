40d7fb0ec75557c43bc3e8cd5eb0a281e5bb564c
Cesar Rodriguez <kesarr@gmail.com>
2018-08-31 11:18:46 +0200

    🚀

--- src/app/components/tweets/tweets.component.ts
+++ src/app/components/tweets/tweets.component.ts
@@ -21,9 +21,6 @@ export class TweetsComponent implements OnInit, OnDestroy {
   public page = 0;
   public stats = null;
   private alive: boolean;
-
-  public msg: string;
-
   public sending = false;
 
   public avatarUrl = null;
