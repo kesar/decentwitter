cd9c4f79281f17a7a720510121ae649a5b156406
Cesar Rodriguez <kesarr@gmail.com>
2019-03-13 01:04:54 +0100

    🚀

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const compression = require('compression');
 const path = require('path');
 const app = express();
+app.use(compression());
 app.use(express.static(__dirname + '/dist'));
 app.get('*', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/index.html'));
